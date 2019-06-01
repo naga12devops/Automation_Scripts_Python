543ab3e8538a0260cb6774e76a5a8c6f8efeedaa
naga12devops <naga12devops@gmail.com>
2019-06-01 10:35:26 +0000

    instal-tomcat-script

---
+++ tomcat-9-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ wget http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.20/bin/apache-tomcat-9.0.20.tar.gz
+ tar -xvzf apache-tomcat-9.0.20.tar.gz
+ rm -fr apache-tomcat-9.0.20.tar.gz
+ cd tar -xvzf apache-tomcat-9.0.20
+ ls -a
+
